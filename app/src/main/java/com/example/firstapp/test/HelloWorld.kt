1505f878cc7432de155114632c409e60f8145362
Ming <ming.cxb@gmail.com>
2021-11-30 18:51:42 +0800

    initial

---
+++ app/src/main/java/com/example/firstapp/test/HelloWorld.kt
@@ -0,0 +1,25 @@
+package com.example.firstapp.test
+
+
+// psvm 快速生成main（）方法
+fun main() {
+    val str1 = "Hello World!"
+    println("Says:${str1}")
+
+
+    val arr = arrayOf(1, 2, 3, 54, 6, 4)
+    println(arr[2])
+
+    val arr2 = arrayOfNulls<String>(5)
+    arr2[0] = "One";
+    arr2[1] = "Two";
+    arr2[2] = "Three";
+    arr2[3] = "Four";
+    arr2[4] = "five";
+
+    for (i in arr2) {
+        println(i)
+    }
+
+
+}
